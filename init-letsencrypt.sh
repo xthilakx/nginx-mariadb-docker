c259aee6c5484b066acf31c762414253bc4282e4
Thilak Rao <rao@thilak.nl>
2019-06-02 13:03:58 +0200

    Add certbot into the same networks as other containers

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -13,7 +13,6 @@ fi
 
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
-mkdir -p "$SSL_CERTS_DATA_DIR/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
